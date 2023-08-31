949b869f089e7d5ba4bb17399f370d37a4a9108e
garrettomi <omigarrett@gmail.com>
2023-08-31 15:35:11 +0900

    Fix:Delete unused code

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-// backend - server.js - Routes API requests from the frontend to Kintone
-
-// Express Server Setup
 const express = require("express");
 const cors = require("cors");
 const PORT = 8000;
 const app = express();
 
-// Hide sensitive info in a .env file with dotenv
 require("dotenv").config({ path: "../.env" });
 
-// Get Kintone credentials from a .env file
 const subdomain = process.env.SUBDOMAIN;
 const appID = process.env.APPID;
 const apiToken = process.env.APITOKEN;
 
-// Parse incoming requests with JSON payloads
 app.use(express.json());
 
-// Set Cross-Origin Resource Sharing (CORS) to frontend React App
 app.use(cors());
 const corsOptions = {
   origin: "http://localhost:3000",
 };
 
-// Kintone's record(s) endpoints
 const multipleRecordsEndpoint = `https://${subdomain}.kintone.com/k/v1/records.json?app=${appID}`;
 const singleRecordEndpoint = `https://${subdomain}.kintone.com/k/v1/record.json?app=${appID}`;
-
-// Kintone app's field codes are 'country' 'state' and 'city'.
 
 app.get("/getData", cors(corsOptions), async (req, res) => {
   const fetchOptions = {
@@ -83,6 +73,6 @@ app.post("/postData", cors(corsOptions), async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(
-    `\n Backend server listening at http://localhost:${PORT} \n Confirm if Kintone records are being retrieved at \n http://localhost:${PORT}/getData`
+    `\n Backend server listening at http://localhost:${PORT} \n Confirm if records are being retrieved at \n http://localhost:${PORT}/getData`
   );
 });
