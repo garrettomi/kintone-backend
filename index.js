3033b8e8d519cce64f470cbf9a8ffb69a71c976f
garrettomi <omigarrett@gmail.com>
2023-09-01 09:04:42 +0900

    Fix: CORS endpoint with HTTPS

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(cors());
 const corsOptions = {
-  origin: "kintone-travel-tracker.vercel.app",
+  origin: "https://kintone-travel-tracker.vercel.app",
 };
 
 const multipleRecordsEndpoint = `https://${subdomain}.kintone.com/k/v1/records.json?app=${appID}`;
